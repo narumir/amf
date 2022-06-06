728e62e5ff8cc0a9c7f42e10af018f919eb8aa5a
ByeongGi <wwww3426@naver.com>
2022-06-06 22:15:52 +0900

    test: add test(deserialize).

---
+++ test/amf-deserialized.spec.ts
@@ -0,0 +1,25 @@
+import {
+    AMF0Deserialize
+} from "src";
+
+test("Deserialize data is 3.5(Number)", () => {
+    const data = Buffer.from([0x00, 0x40, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    const deserialize = new AMF0Deserialize(data);
+    const result = deserialize.readData();
+    expect(result).toEqual(3.5);
+});
+
+test("Deserialize data is true(Boolean)", () => {
+    const data = Buffer.from([0x01, 0x01]);
+    const deserialize = new AMF0Deserialize(data);
+    const result = deserialize.readData();
+    expect(result).toEqual(true);
+});
+
+test("Deserialize data is true(Boolean)", () => {
+    const data = Buffer.from([0x01, 0x01]);
+    const deserialize = new AMF0Deserialize(data);
+    const result = deserialize.readData();
+    expect(result).toEqual(true);
+});
+
